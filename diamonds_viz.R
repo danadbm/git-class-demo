3b7bda8d66d46cbb8aa6c6e12e084b4a20386cf5
danadbm <danadbm@gmail.com>
2023-11-18 09:05:23 -0500

    Added new R visualization

---
+++ diamonds_viz.R
@@ -0,0 +1,12 @@
+### Quick R Script
+### Visualize diamonds dataset
+
+library(ggplot2)
+library(dplyr)
+
+# load data
+data("diamonds")
+
+# visualization
+ggplot(diamonds, aes(x = cut, y = clarity, color = color)) +
+    geom_point()
